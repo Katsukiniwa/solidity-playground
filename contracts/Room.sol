dba48753450f30d5df6cb875a4fff0b881d9cb56
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-06-07 23:44:44 +0900

    fix

--- contracts/Room.sol
+++ contracts/Room.sol
@@ -36,7 +36,7 @@ contract Room is Destructible, Pausable {
   function sendReward(uint256 _reward, address _dest, uint256 _id) external onlyOwner {
     require(!rewardSent[_id]);
     require(_reward > 0);
-    require(address*(this).balance >= _reward);
+    require(address(this).balance >= _reward);
     require(_dest != address(0));
     require(_dest != owner);
 
